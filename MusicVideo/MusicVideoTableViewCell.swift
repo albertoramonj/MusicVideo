b708179b8485ecfd5e6e78b22324fc9ac5ae6069
Alberto R <albertoramon@gmail.com>
2016-04-13 19:42:52 +0200

    Completed Part 15

--- MusicVideo/MusicVideoTableViewCell.swift
+++ MusicVideo/MusicVideoTableViewCell.swift
@@ -23,8 +23,42 @@ class MusicVideoTableViewCell: UITableViewCell {
     @IBOutlet weak var musicTitle: UILabel!
     
     func updateCell() {
-        musicTitle.text = video!.vName
-        rank.text = "\(video!.vRank)"
-        musicImage.image = UIImage(named: "imageNotAvailable")
+        musicTitle.text = video?.vName // Not necessary to unwrap because if it's nil nothing happens, shows blank
+        rank.text = "\(video!.vRank)" // Necessary to unwrap because we are creating a String, if not, it'll shows the string "Optional("xxx")"
+        //musicImage.image = UIImage(named: "imageNotAvailable")
+        
+        if video!.vImageData != nil {
+            print("Get data from array...")
+            musicImage.image = UIImage(data: video!.vImageData!)
+        } else {
+            getVideoImage(video!, imageView: musicImage)
+        }
+        
+        
+        
+    }
+    
+    func getVideoImage(video: Videos, imageView: UIImageView) {
+        // Background thread
+        //  DISPATCH_QUEUE_PRIORITY_HIGH Items dispatched to the queue will run at high priority, i.e. the queue will be scheduled for execution before any default priority or low priority queue.
+        //
+        //  DISPATCH_QUEUE_PRIORITY_DEFAULT Items dispatched to the queue will run at the default priority, i.e. the queue will be scheduled for execution after all high priority queues have been scheduled, but before any low priority queues have been scheduled.
+        //
+        //  DISPATCH_QUEUE_PRIORITY_LOW Items dispatched to the queue will run at low priority, i.e. the queue will be scheduled for execution after all default priority and high priority queues have been scheduled.
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            let data = NSData(contentsOfURL: NSURL(string: video.vImageUrl)!)
+            
+            var image: UIImage?
+            if data != nil {
+                video.vImageData = data
+                image = UIImage(data: data!)
+            }
+            
+            // move to the Main Queue
+            dispatch_async(dispatch_get_main_queue()) {
+                imageView.image = image
+            }
+        }
     }
 }

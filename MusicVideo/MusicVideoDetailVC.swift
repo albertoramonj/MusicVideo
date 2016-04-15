aec0fadeb4c30b8aa838939100ba3fe3d0c89cfd
Alberto R <albertoramon@gmail.com>
2016-04-15 12:52:23 +0200

    Completed Part 19

--- MusicVideo/MusicVideoDetailVC.swift
+++ MusicVideo/MusicVideoDetailVC.swift
@@ -10,4 +10,29 @@ import UIKit
 
 class MusicVideoDetailVC: UIViewController {
 
+    var video:Videos!
+    
+    @IBOutlet weak var vName: UILabel!
+    @IBOutlet weak var videoImage: UIImageView!
+    @IBOutlet weak var vGenre: UILabel!
+    @IBOutlet weak var vPrice: UILabel!
+    @IBOutlet weak var vRights: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = video.vArtist
+        vName.text = video.vName
+        vPrice.text = video.vPrice
+        vRights.text = video.vRights
+        vGenre.text = video.vGenre
+        
+        if video.vImageData != nil {
+            videoImage.image = UIImage(data:video.vImageData!)
+        } else {
+            //No image data available or doing background stuff yet
+            videoImage.image = UIImage(named:"imageNotAvailable")
+        }
+    }
+
 }

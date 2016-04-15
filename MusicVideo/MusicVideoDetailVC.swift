175c7f2a6924668311b79ff9ba7c3788e568ad17
Alberto R <albertoramon@gmail.com>
2016-04-15 12:19:11 +0200

    Completed Part 18

---
+++ MusicVideo/MusicVideoDetailVC.swift
@@ -0,0 +1,13 @@
+//
+//  MusicVideoDetailVC.swift
+//  MusicVideo
+//
+//  Created by Alberto Ramon Janez on 15/4/16.
+//  Copyright © 2016 arj. All rights reserved.
+//
+
+import UIKit
+
+class MusicVideoDetailVC: UIViewController {
+
+}
